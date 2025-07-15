466a05278415ac36ba55a9d8b735420fa2a69759
금가경 <gageum0@gmail.com>
2025-07-15 15:30:26 +0900

    Initial Commit

---
+++ Travel/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Travel
+//
+//  Created by 금가경 on 7/15/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
